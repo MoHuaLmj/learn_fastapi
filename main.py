8e062d6a4708bb4df583632aa3e8acf491e32fbf
shijinghao <shijh@asiainfo-sec.com>
2026-06-29 10:28:02 +0800

    PPS-001 first step

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Optional[bool]
+
+@app.get("/")
+def read_root():
+    return {"hello": "world"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id" : item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_time(item_id: int, item: Item):
+    return {
+        "item_name" : item.name,
+        "item_id" : item_id,
+        "item_price": item.price
+    }
+
